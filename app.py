96563386ea9970f683c70cce8d44a6d73d802d88
01022367829qq-pixel <01022367829qq@gmail.com>
2025-08-08 05:52:56 +0900

    Update app.py

--- app.py
+++ app.py
@@ -529,16 +529,3 @@ st.markdown("""
     @trade_vibes.kr</a> 로 남겨주세요.
 </div>
 """, unsafe_allow_html=True)
-
-# 하단 문구
-st.markdown("<hr>", unsafe_allow_html=True)
-
-st.markdown("<p style='text-align:center; font-size:13px; color:gray;'>Made by Son Jiwan | Powered by Streamlit</p>", unsafe_allow_html=True)
-
-st.markdown("""
-<div style='text-align: center; margin-top: 50px; font-size: 14px; color: #bbbbbb;'>
-    💬 피드백은 <a href="https://www.instagram.com/trade_vibes.kr" target="_blank" style="color: #90caf9; text-decoration: none;">
-    @trade_vibes.kr</a> 로 남겨주세요.
-</div>
-""", unsafe_allow_html=True)
-
